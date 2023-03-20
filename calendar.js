a9fff195b48e40db8c8b973346b32abe9831781c
Ambika-Madan <112340895+Ambika-Madan@users.noreply.github.com>
2023-03-21 01:04:05 +0530

    Appointment

--- calendar.js
+++ calendar.js
@@ -11,3 +11,42 @@ function showCalendar(doctorName) {
       calendar.style.display = "";
     }
   }
+  
+  var min = '2023-03-20T00:00';
+  var max = '2023-09-20T00:00';
+  
+  mobiscroll.datepicker('#demo-booking-single', {
+      controls: ['calendar'],
+      min: min,
+      max: max,
+      onPageLoading: function (event, inst) {
+          getPrices(event.firstDay, function callback(bookings) {
+              inst.setOptions({
+                  labels: bookings.labels,
+                  invalid: bookings.invalid
+              });
+          });
+      }
+  });
+  function getPrices(d, callback) {
+    var invalid = [],
+        labels = [];
+
+    mobiscroll.util.http.getJson('//trial.mobiscroll.com/getprices/?year=' + d.getFullYear() + '&month=' + d.getMonth(), function (bookings) {
+        for (var i = 0; i < bookings.length; ++i) {
+            var booking = bookings[i],
+                d = new Date(booking.d);
+
+            if (booking.price > 0) {
+                labels.push({
+                    start: d,
+                    title: '$' + booking.price,
+                    textColor: '#e1528f'
+                });
+            } else {
+                invalid.push(d);
+            }
+        }
+        callback({ labels: labels, invalid: invalid });
+    }, 'jsonp');
+}
