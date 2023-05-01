33073e3635ed9833e755cd86fd2a2740096c1b1f
Ambika-Madan <112340895+Ambika-Madan@users.noreply.github.com>
2023-05-01 21:07:27 +0530

    database

---
+++ connect1.php
@@ -0,0 +1,34 @@
+<?php
+    $host = "localhost:8012";
+    $dbusername = "root";
+    $dbpassword = "";
+    $dbname = "test1";
+
+    // Database connection
+    $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    if (isset($_POST['save'])) {  
+        $first_name = $_POST['firstName'];
+        $last_name = $_POST['lastName'];
+        $gender = $_POST['gender'];
+        $email = $_POST['email'];
+        $age = $_POST['age'];
+        $phone = $_POST['phone'];
+
+        $sql_query = "INSERT INTO entry_details (first_name, last_name, gender, email, age, mobile)
+            VALUES ('$firstName', '$lastName', '$gender', '$email','$age', '$phone')";
+
+if (mysqli_query($conn, $sql_query)) 
+{
+   echo "New Details Entry inserted successfully !";
+} 
+else
+{
+   echo "Error: " . $sql . "" . mysqli_error($conn);
+}
+mysqli_close($conn);
+}
+?>
